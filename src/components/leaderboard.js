40f7e15a19aa2366feebf209532f5db23eee82cc
mapldx <116471303+mapldx@users.noreply.github.com>
2023-08-20 21:57:39 -0400

    Make changes to section headings

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -29,7 +29,7 @@ const Leaderboard = ({ leaderboard }) => {
         <div className="sm:flex-auto">
           <h1 className="text-base font-semibold leading-6 text-gray-900">Leaderboard</h1>
           <p className="mt-2 text-sm text-gray-700">
-            A list of the top performing strategies sorted by PnL.
+            A list of the top performing strategies sorted by PnL
           </p>
         </div>
       </div>
