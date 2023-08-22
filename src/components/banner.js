1711f6fca672b31cc0b9b82001d77df7fc54aee9
mapldx <116471303+mapldx@users.noreply.github.com>
2023-08-22 02:23:33 -0400

    Fix banner message

--- src/components/banner.js
+++ src/components/banner.js
@@ -5,7 +5,7 @@ export default function Banner() {
         <a href="https://www.cubik.so/project/80aa8a10-a639-44e8-b946-388d1e96372b/hackathon/8e23ade0-0dae-4c4b-83aa-67867749029c" target="_blank">
           <strong className="font-semibold">Kamino One is a submission to LamportDAO&apos;s #OPOSHackathon</strong>
           <span className="px-2">•</span>
-          beta.kamino.one is temporarily optimized for the hackathon. As such, data is set to only refresh every 24h. View submission on Cubik&nbsp;<span aria-hidden="true">&rarr;</span>
+          beta.kamino.one is currently optimized for the hackathon. As such, data is temporarily set to only refresh every 24h. View submission on Cubik&nbsp;<span aria-hidden="true">&rarr;</span>
         </a>
       </p>
       <div className="flex flex-1 justify-end">
